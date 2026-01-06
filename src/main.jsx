b7937b31759e31ad451d0c095d85d435b3d917f0
MicahC-404 <cermelem@gmail.com>
2026-01-06 11:15:34 -0500

    Integrate Redux Provider in main component
    Wrapped the App component with Provider to enable Redux store.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Task 6: Import Provider and the Store
 import { Provider } from 'react-redux'
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap the App component with Provider and pass the store */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
